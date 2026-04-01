7e1ff58d706a6cf5498474f7b4e63544763b08c6
RobinNotBad <robin_0229@qq.com>
2026-04-01 23:33:23 +0800

    优化计算器操作和显示逻辑，现已与正常计算器相同

--- lv_lib_100ask/src/lv_100ask_calc/lv_100ask_calc.c
+++ lv_lib_100ask/src/lv_100ask_calc/lv_100ask_calc.c
@@ -143,19 +143,19 @@ static void lv_100ask_calc_constructor(const lv_obj_class_t * class_p, lv_obj_t 
     //lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
 
     /*Display calculation history and results*/
-    calc->ta_hist = lv_textarea_create(obj);
+    calc->ta_hist  = lv_textarea_create(obj);
     lv_obj_set_style_bg_color(calc->ta_hist, LV_100ASK_COLOR_BLACK, 0);
     lv_obj_set_style_text_color(calc->ta_hist, LV_100ASK_COLOR_GREEN, 0);
     lv_obj_set_style_radius(calc->ta_hist, 0, 0);
+    lv_obj_set_style_border_width(calc->ta_hist, 0, 0);
+    lv_obj_set_size(calc->ta_hist, LV_PCT(100), LV_PCT(9));
 
+    lv_textarea_set_one_line(calc->ta_hist, true);
     lv_textarea_set_cursor_click_pos(calc->ta_hist, false);
     lv_textarea_set_max_length(calc->ta_hist, LV_100ASK_CALC_HISTORY_MAX_LINE);
     lv_obj_set_style_text_align(calc->ta_hist, LV_TEXT_ALIGN_RIGHT, LV_STATE_DEFAULT);
-    lv_textarea_set_one_line(calc->ta_hist, true);
     lv_textarea_set_text(calc->ta_hist, "");
     lv_textarea_set_placeholder_text(calc->ta_hist, "CALC HISTORY");
-    lv_obj_set_style_border_width(calc->ta_hist, 0, 0);
-    lv_obj_set_size(calc->ta_hist, LV_PCT(100), LV_SIZE_CONTENT);
 
     /*Input textarea*/
     calc->ta_input = lv_textarea_create(obj);
@@ -163,10 +163,10 @@ static void lv_100ask_calc_constructor(const lv_obj_class_t * class_p, lv_obj_t 
     lv_obj_set_style_text_color(calc->ta_input, LV_100ASK_COLOR_GREEN, 0);
     lv_obj_set_style_radius(calc->ta_input, 0, 0);
     lv_obj_set_style_border_width(calc->ta_input, 0, 0);
-    lv_obj_set_size(calc->ta_input, LV_PCT(100), LV_SIZE_CONTENT);
+    lv_obj_set_size(calc->ta_input, LV_PCT(100), LV_PCT(9));
 
     lv_textarea_set_one_line(calc->ta_input, true);
-    lv_textarea_set_cursor_click_pos(calc->ta_input, true);
+    lv_textarea_set_cursor_click_pos(calc->ta_input, false);
     lv_textarea_set_max_length(calc->ta_input, LV_100ASK_CALC_HISTORY_MAX_LINE);
     lv_obj_set_style_text_align(calc->ta_input, LV_TEXT_ALIGN_RIGHT, LV_STATE_DEFAULT);
     lv_textarea_set_text(calc->ta_input, "");
@@ -211,6 +211,8 @@ static void calc_btnm_changed_event_cb(lv_event_t *e)
         // Perform operations
         if (strcmp(txt, "=") == 0)
         {
+            if(calc->count <= 0) return;
+
             double calc_result;
 
             // Lexical analyzer
@@ -234,9 +236,10 @@ static void calc_btnm_changed_event_cb(lv_event_t *e)
             else
             {
                 char result_buff[128];
-                sprintf(result_buff, "%s=%.8g", lv_textarea_get_text(calc->ta_input), calc_result);
                 char hist_buff[128];
-                sprintf(hist_buff, "%.8g", calc_result);
+
+                sprintf(hist_buff, "%s=%.8g", lv_textarea_get_text(calc->ta_input), calc_result);
+                sprintf(result_buff, "%.8g", calc_result);
 
                 lv_textarea_set_text(calc->ta_hist, hist_buff);
                 lv_textarea_set_text(calc->ta_input, result_buff);
@@ -258,16 +261,24 @@ static void calc_btnm_changed_event_cb(lv_event_t *e)
         // del char
         else if (strcmp(txt, "<-") == 0)             
         {
-            lv_textarea_del_char(calc->ta_input);
-            calc->calc_exp[calc->count-1] = '\0';
-
-            if(calc->count > 0) calc->count--;
-            else lv_textarea_set_text(calc->ta_input, "");
+            if(calc->count <= 0) {
+                lv_textarea_set_text(calc->ta_input, "");
+            }
+            else {
+                lv_textarea_del_char(calc->ta_input);
+                calc->count--;
+                calc->calc_exp[calc->count] = '\0';
+            }
         }
         // Add char
         else
         {
-            if(calc->count <= 0) lv_textarea_set_text(calc->ta_input, "");
+            if(calc->count <= 0){
+                if(strcmp(txt, "+") || strcmp(txt, "-") || strcmp(txt, "*") || strcmp(txt, "/")) {
+                    strcat(&calc->calc_exp[0], lv_textarea_get_text(calc->ta_input));
+                }
+                else lv_textarea_set_text(calc->ta_input, "");
+            }
 
             lv_textarea_add_text(calc->ta_input, txt);
             strcat(&calc->calc_exp[0], txt);
